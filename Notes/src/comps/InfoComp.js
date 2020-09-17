56ac1033acde8903e6dc75728d0f7b76096209e5
sauravDutt <sauravd069@gmail.com>
2020-09-17 16:34:42 +0530

    Updated some more stuff

--- Notes/src/comps/InfoComp.js
+++ Notes/src/comps/InfoComp.js
@@ -34,10 +34,9 @@ const InfoComp = ({setShowInfo}) => {
                 <div className='post-info'>
                     <h3 className='post-title'>Info</h3>
                     <p>
-                    Notes | SD, are like blog posts on some of the topics of my interest. This application have two features one is the post itself, 
-                    they are just insights in the topics with images and a lot of content about the subject, usefull content all personaly typed by me, the second is the feeds, more like feedback the 
-                    users or anybody who find this application can add a feed and can also brouse other feeds by other users. It's a practice project, build by React. It is a work is Progress and will
-                    always be a work in progress. Thank you for Reading !!
+                    Notes | SD, is a study material. A web application created with an objective of maintaining notes and taking record of all the project ideas.
+                    One stop destination for refreshing all the nessory skills and brushing the knowledge on the key subjects. The app is build with a mobile first 
+                    approach, using React js.Started on the 17th of September,2020. This will always be a work in progress. Thank you for Reading !!
                     </p>
                     <br />
                     <span><button onClick={showHomefrInfo} className="homeBtn">sauravdutt.tech</button></span><span><button onClick={showSkillsfrInfo} className="skillBtn">Explore</button></span>
