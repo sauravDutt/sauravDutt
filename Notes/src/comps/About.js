d2ce5280c96792c0c306965a8d3b33f47467a21c
sauravDutt <sauravd069@gmail.com>
2020-09-28 21:06:15 +0530

    Just added some more stuff

--- Notes/src/comps/About.js
+++ Notes/src/comps/About.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ReactComponent as Github } from '../img/github.svg';
+import { ReactComponent as Resume } from '../img/user.svg';
 
 const About = () => {
     return (
@@ -10,6 +12,14 @@ const About = () => {
                     I am a self taught web developer and a learner, I like to research about latest tech, new 
                     frameworks and polish my skills. I have an insatiable curiosity for AI – particularly deep 
                     learning – applications and their potential to change the technology landscape.
+                </p>
+                <p style={{textAlign:'center',justifyItems:'center', alignItems:'center'}}>
+                    <a href="https://github.com/sauravDutt" target='_blank' rel="noopener noreferrer" className="icon-button">
+                        <span className = "icon-button-git"><Github /></span>
+                    </a>
+                    <a href="https://github.com/sauravDutt" target='_blank' rel="noopener noreferrer" className="icon-button" style={{float:'right',marginTop:'-40px'}}>
+                        <span className = "icon-button-git"><Resume /></span>
+                    </a>
                 </p>
                 <br />
             </div>
