0a539030604a8577edda1d091c1a9c3754a05da3
sauravDutt <sauravd069@gmail.com>
2020-10-23 15:45:16 +0530

    Just added some more shit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Title from './comps/Title';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Title />
     </div>
   );
 }
---
+++ blog/src/comps/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <header className="header-main">
+      <h2>SauravDutt<span className='subLogo'>Blog</span></h2>
+        <a href="https://sauravdutt.tech/"><i className="fas fa-arrow-left" id="headerIcon"></i></a>
+      </header>
+    </div>
+  )
+}
+
+export default Title;
