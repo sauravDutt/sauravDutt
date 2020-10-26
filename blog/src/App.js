1173f88eb69322343f7fb647a870254402367770
sauravDutt <sauravd069@gmail.com>
2020-10-26 14:33:30 +0530

    Just added info

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Title from './comps/Title';
+import Insights from './comps/Insights'
 import './index.css';
 
 function App() {
+
+  const [showInsights, setShowInsights] = useState(null);  
   return (
     <div className="App">
-      <Title />
+      <Title setShowInsights = {setShowInsights}/>
+      {showInsights && <Insights setShowInsights = {setShowInsights}/>}
     </div>
   );
 }
--- blog/src/comps/Title.js
+++ blog/src/comps/Title.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Title = () => {
+const Title = ({setShowInsights}) => {
+
+  const showInfo = () => {
+    setShowInsights(1)
+  }
+
   return (
     <div className="title">
       <header className="header-main">
       <h2>SauravDutt<span className='subLogo'>Blog</span></h2>
         <a href="https://sauravdutt.tech/"><i className="fas fa-arrow-left" id="headerIcon"></i></a>
+        <i className="fas fa-info" id="headerIconInsight" onClick={showInfo}></i>
       </header>
     </div>
   )
